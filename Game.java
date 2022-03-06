44ecbe2660fcf536d61cc688fe6c73bdd075f15f
Felipe <felipemoreira1@gmail.com>
2022-03-06 16:18:19 -0300

    Stage 6/7: New level

--- Game.java
+++ Game.java
@@ -1,17 +1,20 @@
 import java.util.Random;
 public class Game {
     private int size;
-    private long secretCode;
+    private int numberOfPossibleSymbols;
+    private String secretCode;
     private Random random;
+    private String possibleSymbols = "0123456789abcdefghijklmnopqrstuvwxyz";
 
-    public Game(int difficultyLevel) {
+    public Game(int difficultyLevel, int numberOfPossibleSymbols) {
         this.size = difficultyLevel;
+        this.numberOfPossibleSymbols = numberOfPossibleSymbols;
         this.random = new Random();
-        this.secretCode = generateSecretCode(this.size);
+        this.secretCode = generateSecretCode();
     }
 
     // Avalia o palpite
-    public String gradeGuess(long guess) {
+    public String gradeGuess(String guess) {
         int bullsCount = bulls(guess);
         int cowsCount = cows(guess);
 
@@ -23,45 +26,54 @@ public class Game {
     }
 
     // Testa se o palpite foi certeiro
-    public boolean codeGuessed(long guess) {
-        return this.secretCode == guess;
+    public boolean codeGuessed(String guess) {
+        return this.secretCode.equals(guess);
     }
     
     // Gera o código secreto
-    private long generateSecretCode(int size) {
+    private String generateSecretCode() {
 
-        long upperlimit = (long)Math.pow(10, size);
-        long number = (long) (random.nextDouble() * upperlimit);
+        StringBuilder code;
 
         // Gera números até conseguir um que atenda os requisitos
         // de não possuir digitos repetidos e nem começar com o
         // digito 0.
-        while (!checkSecretCode(number, size)) {
-            number = (long) (random.nextDouble() * upperlimit);
+        do {
+            code = new StringBuilder();
+            while (code.length() < this.size) {
+                int pos = random.nextInt(this.numberOfPossibleSymbols);
+                code.append(possibleSymbols.charAt(pos)); 
+            }
+        } while (!checkSecretCode(code.toString()));
+
+        String ast = "";
+        for (int i = 0; i < this.size; i++) {
+            ast += "*";
         }
 
-        return number;
+        String ps = "";
+        if (this.numberOfPossibleSymbols <= 10) {
+            ps = "(0-" + possibleSymbols.charAt(this.numberOfPossibleSymbols - 1) + ")"; 
+        } else if (this.numberOfPossibleSymbols == 11) {
+            ps = "(0-9), a";
+        } else if (this.numberOfPossibleSymbols > 11) {
+            ps = "(0-9), (a-" + possibleSymbols.charAt(this.numberOfPossibleSymbols - 1) + ")"; 
+        }
+        System.out.printf("The secret is prepared: %s %s.\n", ast, ps);
+
+        return code.toString();
     }
 
-
     // Checa se o número gerado é um código válido
-    private boolean checkSecretCode(long code, int size) {
+    private boolean checkSecretCode(String code) {
 
-        //String formattingString = "%0" + size + "d";
-        char[] codeString = String.valueOf(code).toCharArray();
-
-        // O código não pode ser um número iniciado
-        // pelo digito 0.
-        if (codeString.length != size) {
-            return false;
-        }
-
+        char[] codeArray = code.toCharArray();
 
         // Checa por digitos repetidos.
-        for (int i = 0; i < codeString.length; i++) {
-            char c0 = codeString[i];
-            for (int j = i + 1; j < codeString.length; j++) {
-                char c1 = codeString[j];
+        for (int i = 0; i < codeArray.length; i++) {
+            char c0 = codeArray[i];
+            for (int j = i + 1; j < codeArray.length; j++) {
+                char c1 = codeArray[j];
                 if (c0 == c1) {
                     return false;
                 }
@@ -72,12 +84,11 @@ public class Game {
     }
 
     // Conta o número de 'bulls' (digitos corretos e na posição correta)
-    private int bulls(long guess) {
+    private int bulls(String guess) {
         int count = 0;
 
-        char[] sCode = String.valueOf(this.secretCode).toCharArray();
-        String formattingString = "%0" + this.size + "d";
-        char[] guessString = String.format(formattingString, guess).toCharArray();
+        char[] sCode = this.secretCode.toCharArray();
+        char[] guessString = guess.toCharArray();
 
         for (int i = 0; i < sCode.length; i++) {
             if (sCode[i] == guessString[i]) {
@@ -89,12 +100,11 @@ public class Game {
     }
 
     // Conta o número de 'cows' (digitos corretos mas fora de posição)
-    public int cows(long guess) {
+    public int cows(String guess) {
         int count = 0;
 
-        char[] sCode = String.valueOf(this.secretCode).toCharArray();
-        String formattingString = "%0" + this.size + "d";
-        char[] guessString = String.format(formattingString, guess).toCharArray();
+        char[] sCode = this.secretCode.toCharArray();
+        char[] guessString = guess.toCharArray();
 
         for (int i = 0; i < sCode.length; i++) {
             for (int j = 0; j < guessString.length; j++) {
--- Main.java
+++ Main.java
@@ -3,26 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Please, enter the secret code's length:");
+        System.out.println("Input the length of the secret code:");
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
-        while (size > 10) {
+        while (size > 36) {
             System.out.println("Error: can't generate a secret number with a " +
                     "length of " + size + " because there aren't enough unique digits.");
             size = scanner.nextInt();
 
         }
-        Game game = new Game(size);
+
+        System.out.println("Input the number of possible symbols in the code:");
+        int possibleSymbols = scanner.nextInt();
+        while (possibleSymbols > 36) {
+            System.out.println("Error: can't generate a secret number with "
+                    + possibleSymbols + " possible symbols because there aren't enough unique digits.");
+            possibleSymbols = scanner.nextInt();
+
+        }
+        Game game = new Game(size, possibleSymbols);
         System.out.println("Okay, let's start a game!");
 
         int turn = 1;
-        long guess = 0;
-        while (!game.codeGuessed(guess)) {
+        String guess = "";
+        do {
             System.out.println("Turn " + turn + ":");
-            guess = scanner.nextLong();
+            guess = scanner.next();
             String grade = game.gradeGuess(guess);
             System.out.println(grade);
-        }
+            turn++;
+        }while (!game.codeGuessed(guess));
 
         System.out.println("Congratulations! You guessed the secret code.");
 
