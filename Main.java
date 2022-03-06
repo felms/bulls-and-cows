6b6a87d7640b97cfbafcdc8c3734710e099ae532
Felipe <felipemoreira1@gmail.com>
2022-03-06 11:34:10 -0300

    Stage 2/7: Grader

--- Main.java
+++ Main.java
@@ -1,15 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("The secret code is prepared: ****.\n" +
-                "\n" +
-                "Turn 1. Answer:\n" +
-                "1234\n" +
-                "Grade: None.\n" +
-                "\n" +
-                "Turn 2. Answer:\n" +
-                "9876\n" +
-                "Grade: 4 bulls.\n" +
-                "Congrats! The secret code is 9876.");
+        Random random = new Random();
+        int secretCode = random.nextInt(10000);
+        Scanner scanner = new Scanner(System.in);
+        int guess = scanner.nextInt();
+
+        int bullsCount = bulls(secretCode, guess);
+        int cowsCount = cows(secretCode, guess);
+        String grade = bullsCount == 0 && cowsCount == 0 ? "None"
+                : String.format("%s bull(s) and %s cow(s)", bullsCount, cowsCount);
+
+        System.out.println("Grade: " + grade + 
+                ". The secret code is " + String.format("%04d", secretCode) + ".");
+
+        scanner.close();
+
+    }
+
+    public static int bulls(int secretCode, int guess) {
+        int count = 0;
+
+        char[] sCode = String.format("%04d", secretCode).toCharArray();
+        char[] guessString = String.format("%04d", guess).toCharArray();
+
+        for (int i = 0; i < sCode.length; i++) {
+            if (sCode[i] == guessString[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int cows(int secretCode, int guess) {
+        int count = 0;
+
+        char[] sCode = String.format("%04d", secretCode).toCharArray();
+        char[] guessString = String.format("%04d", guess).toCharArray();
+
+        for (int i = 0; i < sCode.length; i++) {
+            for(int j = 0; j < guessString.length; j++) {
+                if (sCode[i] == guessString[j] && i != j) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
