3b21e9874cde055e9da0621effdb62d148c76bb8
Felipe <felipemoreira1@gmail.com>
2022-03-06 22:06:15 -0300

    Stage 7/7: Error!

--- Main.java
+++ Main.java
@@ -3,24 +3,60 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Lê o tamanho do código e testa a validade
+        // do input
         System.out.println("Input the length of the secret code:");
         Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        while (size > 36) {
+        String s = scanner.nextLine();
+
+        int size;
+        try {
+            size = Integer.parseInt(s);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Error: \"" + s + "\" isn't a valid number.");
+            scanner.close();
+            return;
+        }
+
+        if (size > 36 || size < 1) {
             System.out.println("Error: can't generate a secret number with a " +
                     "length of " + size + " because there aren't enough unique digits.");
-            size = scanner.nextInt();
+            scanner.close();
+            return;
 
         }
 
+
+        // Lê o número de símbolos e testa a validade
+        // do input
         System.out.println("Input the number of possible symbols in the code:");
-        int possibleSymbols = scanner.nextInt();
-        while (possibleSymbols > 36) {
+        s = scanner.nextLine();
+        int possibleSymbols;
+        try {
+            possibleSymbols = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: \"" + s + "\" isn't a valid number.");
+            scanner.close();
+            return;
+        }
+
+        if (possibleSymbols < size) {
+            System.out.println("Error: it's not possible to generate a "
+                    + "code with a length of " + size
+                    + " with " + possibleSymbols + "unique symbols.");
+            scanner.close();
+            return;
+        }
+
+        if (possibleSymbols > 36 || possibleSymbols < 1) {
             System.out.println("Error: can't generate a secret number with "
-                    + possibleSymbols + " possible symbols because there aren't enough unique digits.");
-            possibleSymbols = scanner.nextInt();
+                                + possibleSymbols + " digits.");
+            scanner.close();
+            return;
 
         }
+
+        // Cria o codigo e executa o jogo
         Game game = new Game(size, possibleSymbols);
         System.out.println("Okay, let's start a game!");
 
@@ -38,5 +74,5 @@ public class Main {
 
         scanner.close();
     }
-    
+
 }
